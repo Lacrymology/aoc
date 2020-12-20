23bd11e7e1fe249acaf5ad86b6acc57edebdd79c
Tomas Neme <lacrymology@gmail.com>
2020-12-20 13:43:27 +0100

    better organized code

--- event2020/day_8.py
+++ event2020/day_8.py
@@ -9,6 +9,13 @@ class InfiniteLoopError(Exception):
     To be raised when a program detects an infinite loop
     """
 
+    def __init__(self, cp, accum):
+        self.cp = cp
+        self.accum = accum
+
+    def __str__(self):
+        print(f"Infinite Loop Detected: cp: {self.cp}, acc: {self.accum}")
+
 
 class Process:
     def __init__(self, code: list):
@@ -20,19 +27,16 @@ class Process:
 
         self._fixed = False
 
-    def __iter__(self):
+    def execute(self):
         while 0 <= self._cp < len(self._code):
             if self._cp in self._visited_instrs:
-                raise InfiniteLoopError(
-                    f"Infinite Loop Detected: instr: {self._code[self._cp]} cp: {self._cp}, acc: {self._accum}, visited instructions: {self._visited_instrs}"
-                )
+                raise InfiniteLoopError(self._cp, self._accum)
 
             self._visited_instrs.add(self._cp)
-            self.execute()
-            yield self._accum, self._cp
-        yield self._accum, -1
+            self.execute_next()
+        return self._accum
 
-    def execute(self):
+    def execute_next(self):
         instr, param = self._code[self._cp]
         # print(f"{self._cp:0>3d}: {instr} {param}")
 
@@ -54,13 +58,6 @@ class Process:
 with open(path.join(curdir, "day_8.input")) as f:
     code = parse(f, Process.parse_instr)
 
-try:
-    process = Process(code)
-    # will either run the whole program, or raise InfiniteLoopError
-    list(process)
-except InfiniteLoopError as e:
-    print(f"Part 1: {e}")
-
 
 def flip(instr):
     if instr == "nop":
@@ -71,13 +68,35 @@ def flip(instr):
         raise ValueError(instr)
 
 
-for idx, instr in enumerate(code):
-    if instr[0] in ("nop", "jmp"):
-        old = instr[0]
-        code[idx][0] = flip(old)
-        process = Process(code)
-        try:
-            print(f"Part 2: {list(process)}")
-            break
-        except InfiniteLoopError:
-            code[idx][0] = old
+def part_1(code):
+    """
+    Runs the code, and returns the raised exception's accum value if an infinite loop is encountered.
+    Otherwise it'll return None (this would be unexpected)
+    """
+    process = Process(code)
+    try:
+        # will either run the whole program, or raise InfiniteLoopError
+        process.execute()
+    except InfiniteLoopError as e:
+        return e.accum
+
+
+def part_2(code):
+    """
+    Goes through the whole code, each time it encounters a `jmp` or a `nop`, it flips it
+    over and runs the new code. If the process finishes correctly, it returns the returned number,
+    if it raises InfiniteLoopError, it flips back, and goes on trying
+    """
+    for idx, instr in enumerate(code):
+        if instr[0] in ("nop", "jmp"):
+            old = instr[0]
+            code[idx][0] = flip(old)
+            process = Process(code)
+            try:
+                return process.execute()
+            except InfiniteLoopError:
+                code[idx][0] = old
+
+
+print(f"Part 1: {part_1(code)}")
+print(f"Part 2: {part_2(code)}")
